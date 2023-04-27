87296ccd3dfb455e4f394b482d753ebb9ecac5c1
ChristosPeridis <34765945+ChristosPeridis@users.noreply.github.com>
2023-04-27 16:16:31 +0100

    Add files via upload
    Script for reading the .txt logs from the different subfolders of each different agent of DMIU-ShELL Collective and generating the appropriate .csv files, for each of the agents where it stores:
    
    1. The timestamp of the communication
    2. The bite length of the message
    3. The code for the different communication types (0->IDQ,   1->QR,   2->MR,   3->MTR)
    
    The procedure has been optimise to run on multiple CPU cores for efficient execution.

---
+++ txt_to_csv_vz_mc.py
@@ -0,0 +1,29 @@
+import csv
+import os
+import re
+from concurrent.futures import ProcessPoolExecutor
+from tqdm import tqdm
+
+def process_file(file_path):
+    folder_path, file_name = os.path.split(file_path)
+    if file_name.endswith('.txt'):
+        input_file_path = file_path
+        output_file_path = os.path.join(folder_path, file_name.replace('.txt', '.csv'))
+        with open(input_file_path, 'r') as input_file, open(output_file_path, 'w', newline='') as output_file:
+            writer = csv.writer(output_file)
+            writer.writerow(['Timestamp', 'Length', 'Number'])
+            for line in input_file:
+                timestamp = re.search(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}', line)
+                length = re.search(r'length (\d+)', line)
+                number = re.search(r"'(\d+\.\d+\.\d+\.\d+)', \d+, (\d+)", line)
+                if timestamp and length and number:
+                    writer.writerow([timestamp.group(), length.group(1), number.group(2)])
+
+main_folder_path = 'data'
+file_paths = []
+for folder_path, subfolders, file_names in os.walk(main_folder_path):
+    for file_name in file_names:
+        file_paths.append(os.path.join(folder_path, file_name))
+
+with ProcessPoolExecutor() as executor:
+    list(tqdm(executor.map(process_file, file_paths), total=len(file_paths)))
